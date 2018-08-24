2987506e4bbe35aaa3ca5f4ccdb7c6daa0b6d2c8
Xiao Shanghua <hallazie@gmail.com>
2018-08-24 17:46:46 +0800

    add iou and bbox2label

--- util.py
+++ util.py
@@ -9,8 +9,32 @@ import json
 import os
 import numpy as np
 
-def bbox_2_label():
-	pass
+def area(coords):
+	return (coords[1][0]-coords[0][0])*(coords[1][1]-coords[0][1])
+
+def iou(vec1, vec2):
+	'''
+		input ((x0,y0), (x1,y1)), ((x2,y2), (x3,y3))
+		return float
+	'''
+	x0, y0 = max(vec1[0][0], vec2[0][0]), max(vec1[0][1], vec2[0][1])
+	x1, y1 = min(vec1[1][0], vec2[1][0]), min(vec1[1][1], vec2[1][1])
+	intersec = area(((x0,y0), (x1,y1)))
+	print intersec
+	return intersec / float(area(vec1)+area(vec2)-intersec)
+
+def bbox_2_label(input_shape, bbox_json, anchors, num_class, min_down_scale):
+	'''
+		for input_shape=(640,640), len(anchors)=3*3, num_class=16, min_down_scale=8
+		label shape = [(20,20,3,21),(40,40,3,21),(80,80,3,21)]
+		when calculating loss, label will be reshaped to ((80*80*3+40*40*3+20*20*3),21) = (25200,21)
+
+		calculating the max IOU to decide which anchor is the ground truth anchor for each bbox
+	'''
+	anchors_mask = [[i*3+j for j in range(3)] for i in range(3)]
+	grid_shape = [input_shape//{0:min_down_scale, 1:min_down_scale*2, 2:min_down_scale*4}[i] for i in range(3)]
+	label = [np.zeros(grid_shape[i][0],grid_shape[i][1],3,5+num_class) for i in range(3)]
+
 
 def gen_anchor_from_cluster(scale=3, num_anchor=3):
 	coords = []
@@ -33,4 +57,5 @@ def gen_anchor_from_cluster(scale=3, num_anchor=3):
 	return anchors
 
 if __name__ == '__main__':
-	print gen_anchor_from_cluster()
+	# print gen_anchor_from_cluster()
+	pass
