926117cfb11201d6a8f63de70b94091d07705861
Xiao Shanghua <hallazie@gmail.com>
2018-08-29 20:00:16 +0800

    the pipeline is ready, but the training sucks

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 #coding:utf-8
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from sklearn.cluster import *
 
 from config import *
@@ -31,11 +31,13 @@ def sigmoid(x):
 def rect(pannel, bbox, outline=(224,32,128)):
 	draw = ImageDraw.Draw(pannel)
 	w, h = pannel.size
+	font = ImageFont.truetype('font/mriamc.ttf',16)
 	for kv in bbox:
 		pos0, pos1 = bbox[kv]
 		x0, y0, x1, y1 = pos0[0], pos0[1], pos1[0], pos1[1]
-		x0, y0, x1, y1 = x0*(w//WIDTH), y0*(h//HEIGHT), x1*(w//WIDTH), y1*(h//HEIGHT)
+		# x0, y0, x1, y1 = x0*(w//WIDTH), y0*(h//HEIGHT), x1*(w//WIDTH), y1*(h//HEIGHT)
 		draw.polygon([(x0,y0),(x1,y0),(x1,y1),(x0,y1)], outline=outline)
+		draw.text((x0,y0-16),kv,font=font,fill=outline)
 	return pannel
 
 def gav_anchor():
@@ -64,18 +66,19 @@ def label_2_bbox(raw_shape, input_shape, label, anchor, num_class, downscale, th
 	'''
 	w_ratio, h_ratio = float(input_shape[0])/raw_shape[0], float(input_shape[1])/raw_shape[1]
 	anchor = (anchor[0]*w_ratio, anchor[1]*h_ratio)
-	jitter = 1e-7
+	jitter = 1e-10
 	bbox = {}
+	label = label.reshape((input_shape[0]//downscale, input_shape[1]//downscale, 33))
 	for x in range(input_shape[0]//downscale):
 		for y in range(input_shape[1]//downscale):
 			xt, yt, wt, ht = label[x,y,:4]+jitter
-			if label[x,y,4] > threshold:
-				offx, offy = (abs(wt)/wt)*math.e**abs(wt), (abs(ht)/ht)*math.e**abs(ht)
+			if label[x,y,5:].sum() > threshold:
+				offx, offy = (abs(wt)//wt)*math.e**abs(wt), (abs(ht)//ht)*math.e**abs(ht)
 				wa, ha = anchor[0]+offx*2, anchor[1]+offy*2
 				xc, yc = (xt+x)*downscale, (yt+y)*downscale
 				x0, y0, x1, y1 = xc-wa/2, yc-ha/2, xc+wa/2, yc+ha/2
 				idx = list(label[x,y,5:]).index(max(label[x,y,5:]))
-				bbox[BBOX_DICT_REVERSE[idx]] = [[x0,y0],[x1,y1]]
+				bbox[BBOX_DICT_REVERSE[idx]] = [[x0/w_ratio,y0/h_ratio],[x1/w_ratio,y1/h_ratio]]
 	return bbox
 
 def label_transfer(raw_shape, input_shape, label, anchor, downscale):
@@ -108,32 +111,42 @@ def bbox_2_label(raw_shape, input_shape, bbox_json, anchor, num_class, downscale
 		class_idx = BBOX_DICT[bk]
 		bbox = bbox_json[bk]
 		bbox = ((bbox[0][0]*w_ratio, bbox[0][1]*h_ratio), (bbox[1][0]*w_ratio, bbox[1][1]*h_ratio))
-		cur_max_iou, cur_max_x, cur_max_y, same_count = 0, 0, 0, 0
-		iou_dict = []
-		for y in range(grid_shape[1]):
-			for x in range(grid_shape[0]):
-				x0, y0 = x*downscale+downscale/2-anchor[0]/2., y*downscale+downscale/2-anchor[1]/2.
-				x1, y1 = x*downscale+downscale/2+anchor[0]/2., y*downscale+downscale/2+anchor[1]/2.
-				iou = round(calculate_iou(((x0,y0),(x1,y1)), bbox), 3)
-				iou_dict.append((iou,((x,y))))
-				if iou>=cur_max_iou and iou>0:
-					cur_max_iou = iou
-					cur_max_x = x
-					cur_max_y = y
-		x_avg, y_avg, cnt_avg = 0, 0, 0
-		for coord in iou_dict:
-			if coord[0] == cur_max_iou:
-				x_avg += coord[1][0]
-				y_avg += coord[1][1]
-				cnt_avg += 1
-		cur_max_x, cur_max_y = int(x_avg/cnt_avg), int(y_avg/cnt_avg)
-		# transforming
+
+		## calculate IOU and get x, y coords, seems not neccessary for our case.
+		# cur_max_iou, cur_max_x, cur_max_y, same_count = 0, 0, 0, 0
+		# iou_dict = []
+		# for y in range(grid_shape[1]):
+		# 	for x in range(grid_shape[0]):
+		# 		x0, y0 = x*downscale+downscale/2-anchor[0]/2., y*downscale+downscale/2-anchor[1]/2.
+		# 		x1, y1 = x*downscale+downscale/2+anchor[0]/2., y*downscale+downscale/2+anchor[1]/2.
+		# 		iou = round(calculate_iou(((x0,y0),(x1,y1)), bbox), 3)
+		# 		iou_dict.append((iou,((x,y))))
+		# 		if iou>=cur_max_iou and iou>0:
+		# 			cur_max_iou = iou
+		# 			cur_max_x = x
+		# 			cur_max_y = y
+		# x_avg, y_avg, cnt_avg = 0, 0, 0
+		# for coord in iou_dict:
+		# 	if coord[0] == cur_max_iou:
+		# 		x_avg += coord[1][0]
+		# 		y_avg += coord[1][1]
+		# 		cnt_avg += 1
+		# cur_max_x, cur_max_y = int(x_avg/cnt_avg), int(y_avg/cnt_avg)
+		# x0, y0, x1, y1 = bbox[0][0], bbox[0][1], bbox[1][0], bbox[1][1]
+		# xt, yt = ((x0+x1)/2.-cur_max_x*downscale)/downscale, ((y0+y1)/2.-cur_max_y*downscale)/downscale
+		# offx, offy = (abs(x1-x0)-anchor[0])/2., (abs(y1-y0)-anchor[1]/2.)
+		# wt, ht = (offx//abs(offx))*math.log(abs(offx)), (offy//abs(offy))*math.log(abs(offy))
+		# cur_row = [xt, yt, wt, ht, 1]+[1 if i==class_idx else 0 for i in range(num_class)]
+		# label[cur_max_x, cur_max_y] =+ np.array(cur_row)
+
+		## transforming
 		x0, y0, x1, y1 = bbox[0][0], bbox[0][1], bbox[1][0], bbox[1][1]
-		xt, yt = ((x0+x1)/2.-cur_max_x*downscale)/downscale, ((y0+y1)/2.-cur_max_y*downscale)/downscale
-		offx, offy = (abs(x1-x0)-anchor[0])/2., (abs(y1-y0)-anchor[1]/2.)
-		wt, ht = (offx//abs(offx))*math.log(abs(offx)), (offy//abs(offy))*math.log(abs(offy))
-		cur_row = [xt, yt, wt, ht, 1]+[1 if i==class_idx else 0 for i in range(num_class)]
-		label[cur_max_x, cur_max_y] =+ np.array(cur_row)
+		xc, yc = (x0+x1)/2., (y0+y1)/2. 
+		xb, yb = (xc-(xc//downscale)*downscale)/float(downscale), (yc-(yc//downscale)*downscale)/float(downscale)
+		wc, hc = (abs(x1-x0)-anchor[0])/2., (abs(y1-y0)-anchor[1])/2.
+		wb, hb = (wc//abs(wc))*math.log(abs(wc)), (hc//abs(hc))*math.log(abs(hc))
+		cur_row = [xb, yb, wb, hb, 1]+[1 if i==class_idx else 0 for i in range(num_class)]
+		label[xc//downscale, yc//downscale] =+ np.array(cur_row)
 	return label
 
 def label_flatten(input_arr, output_shape):
@@ -147,15 +160,39 @@ def label_flatten(input_arr, output_shape):
 		head += out[0]*out[1]*out[2]
 	return flatten
 
+def diter(train=False):
+	data_path = 'data/imgs/'
+	label_path = 'data/jsons/'
+	anchor = gav_anchor()
+	if train == True:
+		for _,_, fs in os.walk(data_path):
+			fs = sorted(fs)[:10]
+			data = np.zeros((len(fs), 3, WIDTH, HEIGHT))
+			label = np.zeros((len(fs), 33, WIDTH//DOWNSAMPLE, HEIGHT//DOWNSAMPLE))
+			for i, f in enumerate(fs):
+				img = Image.open(data_path+f)
+				raw_size = img.size
+				img = img.resize((WIDTH, HEIGHT), resample=Image.BICUBIC)
+				with open(label_path+f.split('.')[0]+'.json', 'r') as lj:
+					bbox = json.load(lj)
+					res = bbox_2_label(raw_shape=raw_size, input_shape=(WIDTH,HEIGHT), bbox_json=bbox, anchor=anchor, num_class=28, downscale=DOWNSAMPLE)
+				res = res.reshape(((WIDTH//DOWNSAMPLE)*(HEIGHT//DOWNSAMPLE), 33))
+				for line in res:
+					print [round(e,3) for e in line]
+				print '------------------------------------------'
+			print 'data iter gen finished'
+
 if __name__ == '__main__':
 	# with open('data/jsons/0.json', 'r') as jf:
 	# 	bj = json.load(jf)
 	# anchor = gav_anchor()
 	# res = bbox_2_label(raw_shape=(1498,955), input_shape=(640,480), bbox_json=bj, anchor=anchor, num_class=28, downscale=16)
 	# print res.shape
-	# print res.reshape((40*30, 33)).sum(axis=0)
+	# res = res.reshape((40*30, 33))
+	# for e in res:
+	# 	if e[4]>0:
+	# 		print [round(k,3) for k in e]
 	# print '-------------------------------------------------------'
-	# ret = label_transfer(raw_shape=(1498,955), input_shape=(320,240), label=res, anchor=anchor, downscale=8)
-	thresh_np = np.zeros((2, 10, 4, 5))
-	thresh_np[:,1,:,:] = 0.5
-	print thresh_np[0]
+	# ret = label_2_bbox(raw_shape=(1498,955), input_shape=(640,480), label=res, anchor=anchor, num_class=28, downscale=16)
+	# print ret
+	diter(True)
--- yolo.py
+++ yolo.py
@@ -17,19 +17,24 @@ model_prefix = 'params/yolo'
 ctx = mx.gpu(0)
 Batch = namedtuple('Batch', ['data'])
 
+cur_thresh = 0.1
+
 def diter(train=False):
 	data_path = 'data/imgs/'
 	label_path = 'data/jsons/'
 	anchor = util.gav_anchor()
 	if train == True:
 		for _,_, fs in os.walk(data_path):
+			fs = sorted(fs)[:2]
 			data = np.zeros((len(fs), 3, WIDTH, HEIGHT))
 			label = np.zeros((len(fs), 33, WIDTH//DOWNSAMPLE, HEIGHT//DOWNSAMPLE))
 			for i, f in enumerate(fs):
-				img = Image.open(data_path+f).resize((WIDTH, HEIGHT), resample=Image.LANCZOS)
+				img = Image.open(data_path+f)
+				raw_size = img.size
+				img = img.resize((WIDTH, HEIGHT), resample=Image.BICUBIC)
 				with open(label_path+f.split('.')[0]+'.json', 'r') as lj:
 					bbox = json.load(lj)
-					res = util.bbox_2_label(raw_shape=img.size, input_shape=(WIDTH,HEIGHT), bbox_json=bbox, anchor=anchor, num_class=28, downscale=16)
+					res = util.bbox_2_label(raw_shape=raw_size, input_shape=(WIDTH,HEIGHT), bbox_json=bbox, anchor=anchor, num_class=28, downscale=DOWNSAMPLE)
 				data[i] = np.array(img.convert('RGB')).transpose()
 				label[i] = res.transpose().swapaxes(1,2)
 			print 'data iter gen finished'
@@ -48,6 +53,8 @@ def conv_block(data, num_filter, kernel=(3,3), stride=(1,1), pad=(1,1), act_type
 	bn = mx.symbol.BatchNorm(data=conv)
 	if act_type == 'leaky':
 		act = mx.symbol.LeakyReLU(data=bn)
+	elif act_type == 'none':
+		act = bn
 	else:
 		act = mx.symbol.Activation(data=bn, act_type=act_type)
 	return act
@@ -55,42 +62,75 @@ def conv_block(data, num_filter, kernel=(3,3), stride=(1,1), pad=(1,1), act_type
 def pool_block(data, stride=(2,2), kernel=(2,2), pool_type='max'):
 	return mx.symbol.Pooling(data=data, stride=stride, kernel=kernel, pool_type=pool_type)
 
-def confidence_mask(data, threshold, train):
+def confidence_mask_top1_at_each_class(data, threshold, train):
+	# TODO: change to top1 on each class.
 	bsize = BATCH_SIZE if train else 1
-	thresh_np = np.zeros((bsize, 33, WIDTH//DOWNSAMPLE, HEIGHT//DOWNSAMPLE))
-	thresh_np[:,4,:,:] = threshold
-	thresh_in = mx.init.Constant(thresh_np.tolist())
-	thresh_mt = mx.sym.Variable('mask', shape=(bsize, 33, WIDTH//DOWNSAMPLE, HEIGHT//DOWNSAMPLE), init=thresh_in)
-	mask = mx.symbol.broadcast_greater(lhs=data,rhs=thresh_mt)
-	return mask*data
+	obj_vec = mx.symbol.slice(data, begin=(None,5,None,None), end=(None,None,None,None))
+	obk_vec = mx.symbol.reshape(obj_vec, shape=(bsize, 28, (WIDTH//DOWNSAMPLE)*(HEIGHT//DOWNSAMPLE)))
+	top_vec = mx.symbol.topk(obk_vec, axis=2, k=1, ret_typ='mask')
+	rsp_vec = mx.symbol.sum_axis(top_vec, axis=1)
+	tpp_vec = mx.symbol.reshape(rsp_vec, shape=(bsize,1,WIDTH//DOWNSAMPLE,HEIGHT//DOWNSAMPLE))
+	clp_vec = mx.symbol.clip(tpp_vec, 0, 1)
+	blk_vec = mx.symbol.BlockGrad(clp_vec)
+	ret_vec = mx.symbol.broadcast_mul(lhs=data, rhs=blk_vec)
+	return ret_vec, blk_vec
+
+def confidence_mask_thresh(data, threshold, train):
+	bsize = BATCH_SIZE if train else 1
+	obj_vec = mx.symbol.slice(data, begin=(None,4,None,None), end=(None,5,None,None))
+	ths_vec = mx.init.Constant((np.ones((bsize,1,WIDTH//DOWNSAMPLE,HEIGHT//DOWNSAMPLE))*threshold).tolist())
+	tht_vec = mx.sym.Variable('mask', shape=(bsize,1,WIDTH//DOWNSAMPLE,HEIGHT//DOWNSAMPLE), init=ths_vec)
+	rsp_vec = mx.symbol.broadcast_greater(lhs=obj_vec, rhs=tht_vec)
+	blk_vec = mx.symbol.BlockGrad(rsp_vec)
+	return mx.symbol.broadcast_mul(lhs=data, rhs=blk_vec), tht_vec
+
+def diverse_act(data):
+	xy_part = mx.symbol.slice(data, begin=(None,0,None,None), end=(None,2,None,None))
+	wh_part = mx.symbol.slice(data, begin=(None,2,None,None), end=(None,4,None,None))
+	rs_part = mx.symbol.slice(data, begin=(None,4,None,None), end=(None,None,None,None))
+	xy_act = mx.symbol.Activation(xy_part, act_type='sigmoid')
+	rs_act = mx.symbol.Activation(rs_part, act_type='sigmoid')
+	return mx.symbol.concat(xy_act, wh_part, rs_act)
 
 def net(train):
 	# 640*480
 	data = mx.symbol.Variable('data')
 	label = mx.symbol.Variable('softmax_label')
+	# c1 = conv_block(data, 32)
+	# # p1 = pool_block(c1)				
+	# c2 = conv_block(c1, 64)
+	# p2 = pool_block(c2)
+	# r3 = res_block(p2, 128)
+	# r4 = res_block(r3, 128)
+	# p4 = pool_block(r4)
+	# r5 = res_block(p4, 256)
+	# r6 = res_block(r5, 256)
+	# r7 = res_block(r6, 256)
+	# d7 = mx.symbol.Dropout(r7, p=0.2)
+	# p7 = pool_block(d7)
+	# r8 = res_block(p7, 512)
+	# r9 = res_block(r8, 512)
 	c1 = conv_block(data, 32)
-	p1 = pool_block(c1)				
-	c2 = conv_block(p1, 64)
+	c2 = conv_block(c1, 64)
 	p2 = pool_block(c2)
-	r3 = res_block(p2, 128)
-	r4 = res_block(r3, 128)
-	p4 = pool_block(r4)
-	r5 = res_block(p4, 256)
-	r6 = res_block(r5, 256)
-	r7 = res_block(r6, 256)
-	d7 = mx.symbol.Dropout(r7, p=0.4)
-	p7 = pool_block(d7)
-	r8 = res_block(p7, 512)
-	r9 = res_block(r8, 512)
-	d9 = mx.symbol.Dropout(r9, p=0.4)
-	# p9 = pool_block(r9)
-	# r10 = res_block(p9, 512)
-	# r11 = res_block(r10, 512)
-	c12 = conv_block(d9, num_filter=33, kernel=(1,1), stride=(1,1), pad=(0,0), act_type='relu')
-	msk = confidence_mask(c12, 0.5, train)
+	c3 = conv_block(p2, 128)
+	c4 = conv_block(c3, 128)
+	p4 = pool_block(c4)
+	c5 = conv_block(p4, 256)
+	c6 = conv_block(c5, 256)
+	c7 = conv_block(c6, 256)
+	p7 = pool_block(c7)
+	c8 = conv_block(p7, 512)
+	c9 = conv_block(c8, 512)
+	# d9 = mx.symbol.Dropout(r9, p=0.2)
+	c12 = conv_block(c9, num_filter=33, kernel=(1,1), stride=(1,1), pad=(0,0), act_type='none')
+	c13 = diverse_act(c12)
+	msk, rsp_vec = confidence_mask_thresh(c13, THRESHOLD, train)
+
 	if not train:
-		return msk
-	# loss = mx.symbol.LinearRegressionOutput(data=c12, label=label)
+		return mx.symbol.Group([msk, rsp_vec])
+
+	# loss = mx.symbol.LinearRegressionOutput(data=msk, label=label)
 	out_xy = mx.symbol.slice(data=msk, begin=(None,0,None,None), end=(None,2,None,None))
 	out_wh = mx.symbol.slice(data=msk, begin=(None,2,None,None), end=(None,4,None,None))
 	out_oc = mx.symbol.slice(data=msk, begin=(None,4,None,None), end=(None,5,None,None))
@@ -102,7 +142,7 @@ def net(train):
 	xy_loss = mx.symbol.LogisticRegressionOutput(data=out_xy, label=lbl_xy)
 	wh_loss = mx.symbol.LinearRegressionOutput(data=out_wh, label=lbl_wh)
 	oc_loss = mx.symbol.LogisticRegressionOutput(data=out_oc, label=lbl_oc)
-	cs_loss = mx.symbol.LogisticRegressionOutput(data=out_cs, label=lbl_cs)
+	cs_loss = mx.symbol.SoftmaxOutput(data=out_cs, label=lbl_cs)
 	loss = mx.symbol.concat(xy_loss, wh_loss, oc_loss, cs_loss)
 	return loss
 
@@ -114,38 +154,44 @@ def train():
 	model.init_params(initializer=mx.init.Uniform(scale=.1))
 	model.fit(
 		dataiter,
-		optimizer = 'adam',
-		optimizer_params = {'learning_rate':0.1},
+		optimizer = 'adadelta',
+		# optimizer_params = {'learning_rate':0.0005},
+		optimizer_params = {'rho':0.9, 'epsilon':1e-7},
 		eval_metric = 'loss',
-		batch_end_callback = mx.callback.Speedometer(BATCH_SIZE, 1),
-		epoch_end_callback = mx.callback.do_checkpoint(model_prefix, 20),
-		num_epoch = 1000,
+		batch_end_callback = mx.callback.Speedometer(BATCH_SIZE, 5),
+		epoch_end_callback = mx.callback.do_checkpoint(model_prefix, 200),
+		num_epoch = 2000,
 	)
 
 def predict():
 	symbol = net(train=False)
 	anchor = util.gav_anchor()
 	dataiter = diter(train=False)
-	model = mx.mod.Module(symbol=symbol, context=ctx, data_names=('data',), label_names=('softmax_label',))
+	model = mx.mod.Module(symbol=symbol, context=mx.cpu(0), data_names=('data',), label_names=('softmax_label',))
 	model.bind(for_training=False, data_shapes=dataiter.provide_data)
-	_, arg_params, aux_params = mx.model.load_checkpoint(model_prefix, 40)
-	arg_params['mask'] = mx.nd.zeros((1,33,WIDTH//DOWNSAMPLE,HEIGHT//DOWNSAMPLE))
+	_, arg_params, aux_params = mx.model.load_checkpoint(model_prefix, 600)
+	arg_params['mask'] = mx.nd.ones((1,1,WIDTH//DOWNSAMPLE,HEIGHT//DOWNSAMPLE))*THRESHOLD
 	model.set_params(arg_params, aux_params, allow_missing=True)
 	raw = Image.open('data/imgs/0.png').convert('RGB')
 	w, h = raw.size
 	img = np.array(raw.resize((WIDTH, HEIGHT), resample=Image.BICUBIC)).transpose().reshape((1,3,WIDTH,HEIGHT))
 	model.forward(Batch([mx.nd.array(img)]))
-	out = model.get_outputs()[0][0].asnumpy()
-	t = out.reshape(((WIDTH//DOWNSAMPLE)*(HEIGHT//DOWNSAMPLE)),33)
-	for e in t:
-		if e[4]>0:
-			print [round(a,4) for a in e]
+
+	# blk = model.get_outputs()[1][0].asnumpy()
+	# for line in blk[0]:
+	# 	print [round(e,3) for e in line]
+
+	out = model.get_outputs()[0][0].asnumpy().transpose().swapaxes(1,0)
+	for i in range(30):
+		for j in range(22):
+			if True:
+				print '%s,%s--->%s'%(i,j,[round(e,8) for e in out[i,j,:8]])
 	bbox = util.label_2_bbox(raw_shape=(w,h), input_shape=(WIDTH,HEIGHT), label=out, anchor=anchor, num_class=28, downscale=DOWNSAMPLE, threshold=0.1)
 	for k in bbox:
 		print '%s,%s'%(k,bbox[k])
-	util.rect(raw, bbox)
 	print 'predict finished'
-	raw.resize((w,h)).save('data/0.png')
+	raw = util.rect(raw, bbox, outline=(32,128,224))
+	raw.save('data/0.png')
 
 if __name__ == '__main__':
-	train()
+	predict()
